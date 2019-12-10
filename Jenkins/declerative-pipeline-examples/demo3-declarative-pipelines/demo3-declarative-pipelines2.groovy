4022b729eb141237a3ac90109379678aa2e52553
Lev <lev.epstein@thetaray@com>
2019-12-11 00:14:36 +0200

    f

--- Jenkins/declerative-pipeline-examples/demo3-declarative-pipelines/demo3-declarative-pipelines2.groovy
+++ Jenkins/declerative-pipeline-examples/demo3-declarative-pipelines/demo3-declarative-pipelines2.groovy
@@ -1,7 +1,10 @@
+@Library('jenkins-library@master') _
+
 pipeline {
+    agent any
     stages {
         stage ('Git Checkout') {
-          steps {
+        steps {
             gitCheckout(
                 branch: "master",
                 url: "https://github.com/lev-tmp/jenkins2-course-spring-petclinic.git"
