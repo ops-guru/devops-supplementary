678ea93e8cb54886eba66783be21409647f0a832
Lev <lev.epstein@thetaray@com>
2019-12-10 23:50:20 +0200

    comments

--- Jenkins/declerative-pipeline-examples/demo2-declarative-pipelines/demo2-declarative-pipeline4.groovy
+++ Jenkins/declerative-pipeline-examples/demo2-declarative-pipelines/demo2-declarative-pipeline4.groovy
@@ -2,7 +2,12 @@
 // depending on the given condition. The when directive must contain at least one condition. 
 // If the when directive contains more than one condition, all the child conditions must return
 // true for the stage to execute.
-// Create multibranch
+// Create multibranch , https://github.com/levep/multibranch-example.git repository
+// show multibranch principles
+
+/*********
+Do not run on pipline job didn't work
+**********/
 pipeline {
     agent any
     stages {
