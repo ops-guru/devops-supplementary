260393d6526d28c336b54358ba15d62d1c7dd5c2
Lev Epshtein <lev.epstein@gmail.com>
2019-12-11 11:17:48 +0200

    Update demo1-declarative-pipelines4.groovy

--- Jenkins/declerative-pipeline-examples/demo1-declarative-pipelines/demo1-declarative-pipelines4.groovy
+++ Jenkins/declerative-pipeline-examples/demo1-declarative-pipelines/demo1-declarative-pipelines4.groovy
@@ -10,6 +10,7 @@ pipeline {
             steps {
                 sh 'mvn clean package'
                 junit '**/target/surefire-reports/TEST-*.xml'
+                sh 'sleep 3h'
                 archiveArtifacts artifacts: 'target/*.jar', fingerprint: true
             }
         }
